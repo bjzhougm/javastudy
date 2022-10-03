db336fd353c7fb3396e926baa93150a07e9fe910
zhougm <zhouguimin@coocaa.com>
2022-10-03 16:31:26 +0800

    executor 适用实例

---
+++ src/main/java/Factory/simplefactory/Shirt.java
@@ -0,0 +1,9 @@
+package Factory.simplefactory;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public abstract class Shirt {
+    //step1:创建抽象产品类 定义具体产品的公共接口
+    public abstract void Show();
+}
---
+++ src/main/java/multithreaded/threadPoolExecutor/ExecutorServiceTest.java
@@ -0,0 +1,13 @@
+package multithreaded.threadPoolExecutor;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceTest {
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+    }
+
+}
